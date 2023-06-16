9003f2efc490576a63b6477bd3c121d94c1b07dc
TRSaida <tarhanova.saida@yandex.ru>
2023-06-16 19:47:47 +0300

    commit initial

---
+++ js/components/data.js
@@ -0,0 +1,18 @@
+const data = {
+  download: {
+    name: 'download',
+    title: {
+      priority: 1,
+      content: 'Peaceful shopping mindful money',
+    },
+    texts: ['See how we can help with making your shopping experience and money management more Zen'],
+    image: {
+      source: 'assets/images/1.jpg',
+      description: 'Some description for image 1',
+    },
+    links: {
+      appleUrl: 'https://apple.com',
+      googleUrl: 'https://google.com',
+    },
+  },
+};
