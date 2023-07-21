6fef13b6875687cdab65a019ac2909ea3c9e3ebd
TRSaida <tarhanova.saida@yandex.ru>
2023-07-21 14:29:10 +0300

    created SectionPrimary

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log('start');
+import { data } from './components/data.js';
+import { SectionPrimary } from './components/widgets/SPrimary/SectionPrimary.js';
+
+const {download} =data;
+
+const $root = document.querySelector('#root');
+
+$root.insertAdjacentHTML('beforeend', SectionPrimary(download));
+
+console.log($root);
