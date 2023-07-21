197a8d64bfa6b2c20dc91dead81f9207c1d162fa
TRSaida <tarhanova.saida@yandex.ru>
2023-07-21 18:03:53 +0300

    Сorrections made

--- js/components/widgets/SPrimary/SectionPrimary.js
+++ js/components/widgets/SPrimary/SectionPrimary.js
@@ -1,34 +1,33 @@
 import { Title } from '../../ui/Title.js';
-import {Text} from '../../ui/Text/Text.js';
+import { Text } from '../../ui/Text/Text.js';
 import { Image } from '../../ui/Image/Image.js';
 import { BrandLink } from '../../ui/BrandLink/BrandLink.js';
 
-/** @typedef {import ('../../../Schema/Types').SectionPrimary} Data*/
+/** @typedef { import ('../../../Schema/Types').SectionPrimary } Data*/
 
 /**
  * @function SectionPrimary
  * @param {Data} data
- * @returns { string }
+ * @returns {string}
  */
-
 export const SectionPrimary = (data) => {
   const dataKeys = Object.keys(data);
 console.log(dataKeys)
-
 if (dataKeys.length !== 4 && dataKeys.length !== 5) return '';
+
 console.log('Next')
 
 const {name, title, texts, image, links} = data;
 const className = name 
-? name 
-: 'section-primary';
+  ? name 
+  : 'section-primary';
 
 return `
-<section class="${className }"> 
-${title ? Title(title, className) : ''}
-${texts.length > 0 ? texts.map((text) => Text(text, className)).join('') : ''}
-${image ? Image(image, className) : ''}
-${links.length > 0 ? links.map((link) => BrandLink(link,className)).join('') : ''}
-</section>
-`;
+  <section class="${className }"> 
+   ${title ? Title(title, className) : ''}
+   ${texts.length > 0 ? texts.map((text) => Text(text, className)).join('') : ''}
+   ${image ? Image(image, className) : ''}
+   ${links.length > 0 ? links.map((link) => BrandLink(link,className)).join('') : ''}
+  </section>
+  `;
 };
--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 import { data } from './components/data.js';
 import { SectionPrimary } from './components/widgets/SPrimary/SectionPrimary.js';
 
-const {download} =data;
+const { download } = data;
 
 const $root = document.querySelector('#root');
-
 $root.insertAdjacentHTML('beforeend', SectionPrimary(download));
-
 console.log($root);
