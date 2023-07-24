f76a5a1d758f88657466b5660d0235471ec22412
TRSaida <tarhanova.saida@yandex.ru>
2023-07-24 12:13:09 +0300

    Formatted SectionPrimary file

--- js/components/widgets/SPrimary/SectionPrimary.js
+++ js/components/widgets/SPrimary/SectionPrimary.js
@@ -3,7 +3,7 @@ import { Text } from '../../ui/Text/Text.js';
 import { Image } from '../../ui/Image/Image.js';
 import { BrandLink } from '../../ui/BrandLink/BrandLink.js';
 
-/** @typedef {import ('../../../Schema/Types').SectionPrimary} Data */
+/** @typedef {import ('../../../schema/types.js').SectionPrimary} Data */
 
 /**
  * @function SectionPrimary
@@ -15,17 +15,17 @@ export const SectionPrimary = (data) => {
   const dataKeys = Object.keys(data);
   if (dataKeys.length !== 4 && dataKeys.length !== 5) return '';
 
-  const {name, title, texts, image, links} = data;
+  const { name, title, texts, image, links } = data;
   const className = name 
     ? name 
     : 'section-primary';
 
 return `
   <section class="${className }"> 
-   ${title ? Title(title, className) : ''}
-   ${texts.length > 0 ? texts.map((text) => Text(text, className)).join('') : ''}
-   ${image ? Image(image, className) : ''}
-   ${links.length > 0 ? links.map((link) => BrandLink(link,className)).join('') : ''}
+     ${title ? Title(title, className) : ''}
+     ${texts.length > 0 ? texts.map((text) => Text(text, className)).join('') : ''}
+     ${image ? Image(image, className) : ''}
+     ${links.length > 0 ? links.map((link) => BrandLink(link,className)).join('') : ''}
   </section>
   `;
 };
